384e138f48d61ec80e86f5635b6654af3792bc2a
admin <admin@admins-macbook-pro.local>
2018-09-13 10:20:27 -0400

    Initial Commit

---
+++ swiftFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftFun
+//
+//  Created by admin on 9/13/18.
+//  Copyright © 2018 RobPecoraro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
