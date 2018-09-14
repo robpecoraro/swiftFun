4708e3cafa1372ce3671b5901e592aba226e2141
Rob Pecoraro <rfpecoraro@gmail.com>
2018-09-13 21:34:11 -0400

    FinalAppCalculator
    Final App for "janky" calculator

--- swiftFun/ViewController.swift
+++ swiftFun/ViewController.swift
@@ -9,29 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var buttonCount = 0
+   
     
     @IBOutlet var upperTextBox: UITextField!
     @IBOutlet var myLabel: UILabel!
     @IBOutlet var lowerTextBox: UITextField!
+    @IBOutlet var additionSwitch: UISwitch!
     
     @IBAction func buttonPress(_ sender: Any) {
-       let sum = Int(upperTextBox.text!)! + Int(lowerTextBox.text!)!
-        
-        myLabel.text = "\(upperTextBox.text!) + \(lowerTextBox.text!) = \(sum)"
-        
-        
-        /*
-        buttonCount += 1
-        view.backgroundColor = UIColor.blue
-        myLabel.text = "You did this yourself."
-        if buttonCount >= 10 {
-            // This changes the back ground.
-            view.backgroundColor = UIColor.red
-            // This changes the label
-            myLabel.text = "You used a if statement."
+       let addition = additionSwitch.isOn
+        if addition {
+            let sum = Double(upperTextBox.text!)! + Double(lowerTextBox.text!)!
+            
+            myLabel.text = "\(upperTextBox.text!) + \(lowerTextBox.text! ) = \(sum)"
+        } else {
+            let sum = Double(upperTextBox.text!)! - Double(lowerTextBox.text!)!
+            myLabel.text = "\(upperTextBox.text!) - \(lowerTextBox.text!) = \(sum)"
         }
-        */
+     
     }
     override func viewDidLoad() {
         super.viewDidLoad()
